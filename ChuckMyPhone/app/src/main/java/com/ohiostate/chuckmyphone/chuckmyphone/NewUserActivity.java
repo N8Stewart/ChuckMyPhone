f6dc5118b0af112aaf24029a0cb9f722ec4ea25c
Tim Taylor <taylor.2206@osu.edu>
2016-02-08 20:12:41 -0800

    Added new user activity
    added the screen for the new user login page

---
+++ ChuckMyPhone/app/src/main/java/com/ohiostate/chuckmyphone/chuckmyphone/NewUserActivity.java
@@ -0,0 +1,23 @@
+package com.ohiostate.chuckmyphone.chuckmyphone;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class NewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_user);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //Set title at top of screen to "New User"
+        getSupportActionBar().setTitle("New User");
+    }
+
+}
