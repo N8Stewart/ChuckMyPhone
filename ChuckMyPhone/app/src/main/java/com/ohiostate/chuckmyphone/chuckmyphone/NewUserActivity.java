fb9b951df8dea7dc67a975ecdada068945c5950f
N8Stewart <Nate.Stewart12@gmail.com>
2016-03-28 20:36:23 -0400

    Widen username thresholds to 3-12.

--- ChuckMyPhone/app/src/main/java/com/ohiostate/chuckmyphone/chuckmyphone/NewUserActivity.java
+++ ChuckMyPhone/app/src/main/java/com/ohiostate/chuckmyphone/chuckmyphone/NewUserActivity.java
@@ -15,8 +15,8 @@ import android.widget.Toast;
 
 public class NewUserActivity extends AppCompatActivity implements View.OnClickListener{
 
-    public final int USERNAME_LENGTH_MIN = 4;
-    public final int USERNAME_lENGTH_MAX = 10;
+    public final int USERNAME_LENGTH_MIN = 3;
+    public final int USERNAME_lENGTH_MAX = 12;
 
     private final String TAG = this.getClass().getSimpleName();
 
